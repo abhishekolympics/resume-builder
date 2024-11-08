4091c7229bf9d72cd0516c16e207358d2b1cdcad
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-11-08 23:55:51 +0530

    add new name for profile page handler

--- frontend/src/components/Utils/Navbar.js
+++ frontend/src/components/Utils/Navbar.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./navbar.css";
 
-const Navbar = ({ username, onLogout, showLogout, onCreateTask }) => {
+const Navbar = ({ username, onLogout, showLogout, goToProfilePage }) => {
   return (
     <nav className="navbar">
-      {showLogout ?? (
+      {showLogout && (
         <div className="navbar-left">
-          <button onClick={onCreateTask}>User Profile</button>
+          <button onClick={goToProfilePage}>User Profile</button>
         </div>
       )}
       <div className="navbar-center">
         <h1 className="navbar-title">Jobs</h1>
       </div>
-      {showLogout ?? (
+      {showLogout && (
         <div className="navbar-right">
           <span>{username}</span>
           <button onClick={onLogout}>Logout</button>
