f5966ec5d540b11b37b9dccf048d32fa24ce30d2
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-11-10 13:31:43 +0530

    add route to share profile

---
+++ backend/controllers/shareProfile.js
@@ -0,0 +1,26 @@
+const Resume = require("../models/Resume");
+
+const shareProfile = async (req, res) => {
+  try {
+    const { email } = req.query; // Use query to get email
+    console.log("received email =", email);
+
+    if (!email) {
+      return res.status(400).json({ message: "Email is required" });
+    }
+
+    // Assuming you're looking for exact matches on email
+    const profile = await Resume.find({ email }); // Correct query to find based on email
+    if (profile.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No details found for this email." });
+    }
+
+    res.status(200).json({ profile });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving profile", error });
+  }
+};
+
+module.exports = { shareProfile };
--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { saveResume } = require('../controllers/resumeController');
+const { shareProfile} = require('../controllers/shareProfile.js');
 
 const router = express.Router();
 
 router.post('/save-resume', saveResume);
+
+router.get('/profile',shareProfile);
 
 module.exports = router;
