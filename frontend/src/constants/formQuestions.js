f85b231f501b6efa0ba01179b8a19d16f6881607
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-11-12 23:59:07 +0530

    add new timings for the questions

--- frontend/src/constants/formQuestions.js
+++ frontend/src/constants/formQuestions.js
@@ -5,7 +5,7 @@ export const questions = [
   "What are your strongest skills related to this role?",
   "Where did you work most recently, and what was your role?",
   "What were your main responsibilities at your last job?",
-  "Could you describe your educational background?",
+  "Could you describe your educational background? (degree, university)",
   "What certifications or additional training do you have?",
   "How many years of experience do you have in your field?",
   "What software or tools are you proficient in?",
@@ -13,15 +13,15 @@ export const questions = [
 ];
 
 export const maxRecordingTimes = [
-  5000, // 20 seconds for "What is your full name?"
-  5000, // 20 seconds for "What job title are you aiming for?"
-  10000, // 25 seconds for "Could you briefly describe your career objective?"
-  10000, // 30 seconds for "What are your strongest skills related to this role?"
-  5000, // 30 seconds for "Where did you work most recently, and what was your role?"
-  10000, // 35 seconds for "What were your main responsibilities at your last job?"
+  7000, // 20 seconds for "What is your full name?"
+  10000, // 20 seconds for "What job title are you aiming for?"
+  15000, // 25 seconds for "Could you briefly describe your career objective?"
+  15000, // 30 seconds for "What are your strongest skills related to this role?"
+  12000, // 30 seconds for "Where did you work most recently, and what was your role?"
+  15000, // 35 seconds for "What were your main responsibilities at your last job?"
   15000, // 30 seconds for "Could you describe your educational background?"
-  5000, // 25 seconds for "What certifications or additional training do you have?"
-  5000, // 20 seconds for "How many years of experience do you have in your field?"
-  10000, // 25 seconds for "What software or tools are you proficient in?"
-  20000, // 20 seconds for "How can employers contact you (email, phone number)?"
+  15000, // 25 seconds for "What certifications or additional training do you have?"
+  7000, // 20 seconds for "How many years of experience do you have in your field?"
+  20000, // 25 seconds for "What software or tools are you proficient in?"
+  25000, // 20 seconds for "How can employers contact you (email, phone number)?"
 ];
