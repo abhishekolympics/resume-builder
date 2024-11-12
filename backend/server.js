2dba8eb9486ec0a1621859e41b13de55efba468e
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-11-12 19:09:57 +0530

    add deployed frontend url

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://audio-resume-generator.up.railway.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
