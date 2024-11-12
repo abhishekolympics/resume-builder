087807d1d93971eee8fd6031727f56b3d3bdac6f
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-11-12 16:41:37 +0530

    restructure Resume schema

--- backend/models/Resume.js
+++ backend/models/Resume.js
@@ -1,34 +1,48 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define the schema for the resume data
-const resumeSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  email: { type: String, required: true },
-  careerObjective: { type: String, required: true },
-  skills: { type: [String], required: true },
-  experience: [{
-    company: String,
-    position: String,
-    startDate: Date,
-    endDate: Date,
-    responsibilities: [String]
-  }],
-  education: [{
-    degree: String,
-    institution: String,
-  }],
-  projects: [{
-    projectName: String,
-    description: String,
-    technologiesUsed: [String]
-  }],
-  contactInfo: {
-    phoneNumber: String,
-    address: String
-  }
-}, { timestamps: true });
+const resumeSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
+    jobTitle: { type: String },
+    careerObjective: { type: String },
+    skills: { type: [String] },
+    recentJob: { type: String },
+    experience: [
+      {
+        company: String,
+        position: String,
+        startDate: Date,
+        endDate: Date,
+        responsibilities: String,
+      },
+    ],
+    education: [
+      {
+        degree: String,
+        institution: String,
+      },
+    ],
+    certifications: { type: String },
+    yearOfExperience: { type: String },
+    proficiency: { type: String },
+    projects: [
+      {
+        projectName: String,
+        description: String,
+        technologiesUsed: [String],
+      },
+    ],
+    contactInfo: {
+      phoneNumber: String,
+      emailAddress: String,
+    },
+  },
+  { timestamps: true }
+);
 
 // Create a model using the schema
-const Resume = mongoose.model('Resume', resumeSchema);
+const Resume = mongoose.model("Resume", resumeSchema);
 
 module.exports = Resume;
