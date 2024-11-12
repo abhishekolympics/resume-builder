1dcae863f9f869c45997ed3dc78f553c75b6b22c
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-11-12 21:38:14 +0530

    add questions and timers in a constant file

---
+++ frontend/src/constants/formQuestions.js
@@ -0,0 +1,27 @@
+export const questions = [
+  "What is your full name?",
+  "What job title are you aiming for?",
+  "Could you briefly describe your career objective?",
+  "What are your strongest skills related to this role?",
+  "Where did you work most recently, and what was your role?",
+  "What were your main responsibilities at your last job?",
+  "Could you describe your educational background?",
+  "What certifications or additional training do you have?",
+  "How many years of experience do you have in your field?",
+  "What software or tools are you proficient in?",
+  "How can employers contact you (email, phone number)?",
+];
+
+export const maxRecordingTimes = [
+  5000, // 20 seconds for "What is your full name?"
+  5000, // 20 seconds for "What job title are you aiming for?"
+  10000, // 25 seconds for "Could you briefly describe your career objective?"
+  10000, // 30 seconds for "What are your strongest skills related to this role?"
+  5000, // 30 seconds for "Where did you work most recently, and what was your role?"
+  10000, // 35 seconds for "What were your main responsibilities at your last job?"
+  15000, // 30 seconds for "Could you describe your educational background?"
+  5000, // 25 seconds for "What certifications or additional training do you have?"
+  5000, // 20 seconds for "How many years of experience do you have in your field?"
+  10000, // 25 seconds for "What software or tools are you proficient in?"
+  20000, // 20 seconds for "How can employers contact you (email, phone number)?"
+];
