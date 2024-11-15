2a57ca52c7bfc3f770ad17fc8921b6fbfc3ed479
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-11-15 15:39:24 +0530

    remove required from userId in Resume model

--- backend/models/Resume.js
+++ backend/models/Resume.js
@@ -41,7 +41,6 @@ const resumeSchema = new mongoose.Schema(
     userid: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
   },
   { timestamps: true }
